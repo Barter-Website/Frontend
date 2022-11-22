87771e0c4927a9cd6ee31184ae6ab5e3e649eca0
htchoi1006 <htchoi1006@kookmin.ac.kr>
2022-11-22 15:10:31 +0900

    회원가입 페이지 구현

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import SignUpPage from './pages/SignUpPage';
+import SignInPage from './pages/SignInPage';
 
 const App = () => {
   return (
@@ -11,7 +12,9 @@ const App = () => {
         {/* 회원가입 페이지 */}
         <Route path="/" element={<SignUpPage />} />
 
-        
+        {/* 회원가입 페이지 */}
+        <Route path="/signinpage" element={<SignInPage />} />
+
 
       </Routes>
     </BrowserRouter>
---
+++ vite-project/src/pages/SignInPage/index.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import * as styled from './styles';
+
+
+const SignInPage = () => {
+    return(
+        <styled.Container>
+            hello world
+        </styled.Container>        
+    )
+}
+
+export default SignInPage;
