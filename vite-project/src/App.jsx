088ee41ba1db29a08f2ecc20d340f35faf2173f5
BORI <bohhh_@naver.com>
2022-12-25 23:13:55 +0900

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import ChattingPage from './pages/ChattingPage';
 
 import SignUpPage from './pages/SignUpPage';
 import SignInPage from './pages/SignInPage';
